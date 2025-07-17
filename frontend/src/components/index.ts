a6b77a3687ca6b48679c742e175a96797cad24e8
fightTone <qgee123@gmail.com>
2025-07-17 12:14:07 +0800

    � fixes

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,1 +1,10 @@
 // Export all components from this file for easier imports
+export * from './Layout';
+
+// Add more component exports here as they are created
+// For example:
+// export * from './Button';
+// export * from './Card';
+
+// This ensures the file is treated as a module
+export {};
